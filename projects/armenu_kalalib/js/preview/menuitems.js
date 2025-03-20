0b276717b8f0d5991f15935686099a0f41cdb4dd
Gameboy612 <lokapakki@gmail.com>
2025-03-20 13:43:48 +0800

    fixed menu items

--- projects/armenu_kalalib/js/preview/menuitems.js
+++ projects/armenu_kalalib/js/preview/menuitems.js
@@ -7,40 +7,41 @@ const urlParams = new URLSearchParams(queryString);
 
 const menu = [
     {
-        "title": "Boba Icy Yum",
-        "description": "Delicious!",
-        "price": 49.0,
-        "model": "data/16_3_2025.glb"
-    },
-    {
-        "title": "Chewy Icy Yum",
-        "description": "Chewy!",
-        "price": 49.0,
-        "model": "data/16_3_2025(1).glb"
-    },
-    {
-        "title": "Sharp Icy Yumm",
-        "description": "OOhhh!",
-        "price": 49.0,
-        "model": "data/16_3_2025(2).glb"
-    },
-    {
         "title": "Premium Congee",
         "description": "Delicious!",
         "price": 49.0,
-        "model": "data/compressed_2_4_2025.glb"
+        "model": "data/compressed_2_4_2025.glb",
+        "image":"https://media-hosting.imagekit.io//48bc374b85b34974/%E4%B8%8B%E8%BC%89%20(1).jpeg?Expires=1837015293&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=JnXks9Cd2sL-WL~CrIYFWEwbsLIrlvzRWNQevIMIcABKFueQQsiaUIHQ0jQdvUgQB9zno05mMvMGqR5wRxi9SlAy9J5HjL9P-Zas3y5jvJQWjNp28A-V83bNVIC~JRxmHe9jpiAq56fyixHiKGHZMyzVygaCA4T7YJwh57CJbxOM0zwGAM8RRhHYOfcJPKhJJCm2o6h560-Dk7S7TjLi8CnmxEc3AvDkvr5enWfJlgp6n-HdM9boIGQK0pGare6SudKm3JjbcWtfbYtkjSHz-gRck-SBh8bokQ6t8QA3CkThlv~cYqbiIvmoEZpFETj0PQwg3DHQh~O34jy3cmLKlg__" //the image download from web for testing only
     },
     {
         "title": "Deluxe Taco",
         "description": "Yummy!",
         "price": 39.0,
-        "model": "data/compressed_2_19_2025.glb"
+        "model": "data/compressed_2_19_2025.glb",
+        "image":"https://media-hosting.imagekit.io//9086b19750314ddc/%E4%B8%8B%E8%BC%89%20(2).jpeg?Expires=1837016800&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=rzWeeB9GbvdXWPVt9gBNSZbD1eo-chZMIMe~o57vF3YXjLZROOgPWN9IoPxlH4H5zqriz8zzB3CoIyV8QpBlxl7w2aPDM2khUSFtbZvU87c6VvvmgjsA6xlCYAp~5Av9bsGzn6ppPVoxE7ODkcT5S--dhoX5OodXWZwq-bf~7V7vF3RdeS3UCAtcrWDNbLlDcZZAqkERCkLF-t3mJHCKIbaotVoT2-4baoy~mVUA3cC4HXyk1t3EceZyuznmNounPJQqZ22jUsY6YVGkfpfNG3rIuuIB8unJ8k3YqzHuz887AkQEl4-XOFhFB9HcCx-W4DxRZwW8qU5TaGlgk2ztrw__" //the image download from web for testing only
+    },
+    {
+        "title": "Chewy Icy Yum",
+        "description": "Chewy!",
+        "price": 49.0,
+        "model": "data/3月16日 下午9_19 (1).glb",
+        "image": "https://media.discordapp.net/attachments/738532982985457724/1352154460432105502/IMG-20250318-WA0001.jpg?ex=67dcfb79&is=67dba9f9&hm=f5f86d846b5bab04f9d3849ce5cb904657b108ac8425492c9e579c61d9fcbeaf&=&format=webp&width=972&height=1296"
+
+    },
+    {
+        "title": "Boba Icy Yumm",
+        "description": "OOhhh!",
+        "price": 49.0,
+        "model": "data/3月16日 下午9_28 (1).glb",
+        "image": "https://media.discordapp.net/attachments/738532982985457724/1352154563691548672/IMG_20250316_212221.jpg?ex=67dcfb91&is=67dbaa11&hm=cf820d10edf22e8e38db9e12ac673483bc43f500f1b01229e5db9dabdb46534e&=&format=webp&width=972&height=1296"
     },
     {
         "title": "Choco Butter Bun",
         "description": "UM Deluxe",
         "price": 39.0,
-        "model": "data/3_18_2025.glb"
+        "model": "data/3_18_2025.glb",
+        "image":"https://cdn.discordapp.com/attachments/738532982985457724/1352155531296505917/image.png?ex=67dcfc78&is=67dbaaf8&hm=c7c427679644524f73df0868d4fb740d68367adcf3466505a7dd016f411eb41b&" //the image download from web for testing only
+
     }
 ];
 
@@ -60,7 +61,10 @@ class ModelManipulator {
         
         document.getElementById("description").innerText = this.menu[this.modelId].description;
         
-        document.getElementById("price_text").innerText = `$ ${this.menu[this.modelId].price}`;
+        document.getElementById("price_text").innerText = `$ ${this.menu[this.modelId].price} \nOrder`;
+
+        document.getElementById("header_image").src = this.menu[this.modelId].image;
+
     }
 
     nextModel() {
@@ -110,5 +114,3 @@ modelManipulator.renderMenu();
 //         modelManipulator.nextModel();
 //     }
 // })
-
-
